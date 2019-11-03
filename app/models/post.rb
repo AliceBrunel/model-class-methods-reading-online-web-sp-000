4a2426383634965c7434006682f161476a944381
Alice Brunel <alc.brunel@gmail.com>
2019-11-03 15:15:11 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,13 +18,4 @@ class Post < ActiveRecord::Base
     self.title = self.title.titlecase
   end
 
-  def author_name
-    self.author.name if self.author
-  end
-
-  def author_name=(author)
-    author = Post.find_or_create_by(name: name)
-    self.author = author
-  end
-
 end
